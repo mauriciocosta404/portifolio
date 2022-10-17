b8126d2da1ff0b8c870ffa4b515c467ab7184210
menezescosta <mc611641@gmail.com>
2022-10-17 22:34:13 +0100

    feat: about feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';  
 import NavBar from './components/navBar';
 import Hero from './components/hero';
+import Analytics from './components/analytics';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Hero/>
+      <Analytics/>
     </div>
   );
 }
