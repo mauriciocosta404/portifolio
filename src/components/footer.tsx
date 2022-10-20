b9ecc0559f7730d938f9c555665b762a49c68870
menezescosta <mc611641@gmail.com>
2022-10-20 04:24:48 +0100

    feat: adicionando algumas informacoes extras

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,7 +16,7 @@ const Footer = ({ nav, setNav }: NavProps) => {
         <div onClick={() => { setNav(false) }} id='details' className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
             <div>
                 <h1 className="w-full text-3xl font-bold text-[#00df9a]">Maurício</h1>
-                <p className='py-4 '>Lorem ipsum dolor sit amet consectetur rem tempore est. Voluptatem non sequi in molestias at accusamus nobis nulla architecto repellat perspiciatis.</p>
+                <p className='py-4 '>I'm currently studying Frontend development and some other technologies that complement this process, such as HTML, CSS, JS, TS, React, Next, Node, Git and GitHub, some CSS Frameworks like Tailwind, scss . My goal is to become a competent, relevant professional in the technology area.</p>
                 <div className='flex md:w-[75%] my-6 justify-between'>
                     <FaFacebookSquare size={30}/>
                     <a href="https://www.linkedin.com/in/mauricio-costa-216bb1235">
@@ -30,6 +30,15 @@ const Footer = ({ nav, setNav }: NavProps) => {
                 </div>
             </div>
             <div className='lg:col-span-2 flex justify-between mt-6'>
+                <div className='lg:ml-8'>
+                    <h6 className=' font-medium text-gray-400'>Perfil</h6>
+                    <ul>
+                        <li className='py-2 text-sm '>email:mc611641@gmail.com</li>
+                        <li className='py-2 text-sm '>whatsapp:941692901</li>
+                        <li className='py-2 text-sm '>telefone:943377863</li>
+                        <li className='py-2 text-sm '></li>
+                    </ul>
+                </div>
                 <div>
                     <h6 className='font-medium text-gray-400'>Qualities</h6>
                     <ul>
@@ -40,15 +49,6 @@ const Footer = ({ nav, setNav }: NavProps) => {
                     </ul>
                 </div>
                 <div>
-                    <h6 className='font-medium text-gray-400'>Perfil</h6>
-                    <ul>
-                        <li className='py-2 text-sm '>one-off</li>
-                        <li className='py-2 text-sm '>hardworking</li>
-                        <li className='py-2 text-sm '>communicative</li>
-                        <li className='py-2 text-sm '>Flexible</li>
-                    </ul>
-                </div>
-                <div>
                     <h6 className='font-medium text-gray-400'>Likes</h6>
                     <ul>
                         <li className='py-2 text-sm '>one-off</li>
@@ -57,15 +57,7 @@ const Footer = ({ nav, setNav }: NavProps) => {
                         <li className='py-2 text-sm '>Flexible</li>
                     </ul>
                 </div>
-                <div>
-                    <h6 className='font-medium text-gray-400'>Likes</h6>
-                    <ul>
-                        <li className='py-2 text-sm '>one-off</li>
-                        <li className='py-2 text-sm '>hardworking</li>
-                        <li className='py-2 text-sm '>communicative</li>
-                        <li className='py-2 text-sm '>Flexible</li>
-                    </ul>
-                </div>
+      
             </div>
         </div>
     )
