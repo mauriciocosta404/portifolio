f70884a5081c4d476583e50657362e56d74e52fa
menezescosta <mc611641@gmail.com>
2023-02-06 23:06:05 +0100

    feat: adicionando botão download resume

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,5 +1,4 @@
 import Laptop from '../assets/js.jpg';
-
 interface NavProps {
     nav: boolean;
     setNav(nav: boolean): void;
@@ -15,7 +14,7 @@ const Analytics = ({ nav, setNav }: NavProps)=>{
                     <p className='text-[#00df9a]'>MAURICIO ANTÓNIO DA COSTA</p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>About my job as front-end</h1>
                     <p className='text-justify'>I'm a Frontend Developer, ambitious, hyperfocused and looking to grow in the area. I've always been a self-taught student/researcher in several areas (I reap many benefits to this day) but now I'm pointing out everything I have and dedicating myself to the fullest in the digital field.</p>
-                    <button className='bg-black text-[#00df9a] w-[200px] rounded-md mx-auto  md:mx-0 font-medium my-6 p-2'><a href='../../public/MauricioCV.pdf' download>Download CV </a></button>
+                    <a className='text-center bg-black text-[#00df9a] w-[200px] rounded-md mx-auto  md:mx-0 font-medium my-6 p-2'  href={require('../assets/resume/resume.pdf')} download='resume.pdf'>Download CV</a>
                 </div>
             </div>
         </div>
